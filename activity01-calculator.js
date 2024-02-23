4bfa806a87940936b1d33ea471b37b7ecf0d0c16
chubbyle <chubbyle@gmail.com>
2024-02-23 22:32:06 +0800

    added solution using switch.case

--- activity01-calculator.js
+++ activity01-calculator.js
@@ -29,3 +29,38 @@ function calculate(firstNumber, operator, secondNumber) {
 calculate(firstNumber, operator, secondNumber);
 
 //Solution using SWITCH.CASE
+
+// let num1 = prompt("Please enter a number");
+// num1 = Number(num1);
+// let num2 = prompt("Please enter a second number");
+// num2 = Number(num2);
+// let operand = prompt(
+//   "Please enter the operand to be used for calculation ( + . - , * , / )"
+// );
+
+// function calculator(num1, num2, operand) {
+//   let result;
+//   switch (operand) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       if (num2 != 0) {
+//         result = num1 / num2;
+//       } else {
+//         return "Error: Division by zero is not allowed.";
+//       }
+//       break;
+//     default:
+//       return "Error: Invalid operand.";
+//   }
+//   return result;
+// }
+
+// console.log(calculator(num1, num2, operand));
